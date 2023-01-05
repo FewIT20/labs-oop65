ce8cbb91d68e172e9a775dd44c2e802f3b51509f
EntityKunG <sharpkung4826@gmail.com>
2023-01-05 18:19:50 +0700

    Add week 6

---
+++ src/week6/ปสดกับWeek6เดะมาทำ.java
@@ -0,0 +1,13 @@
+/**
+ * You can follow me on instagram!
+ * https://www.instagram.com/few.pz/
+ */
+package week6;
+
+/**
+ *
+ * @author "FewPz (IG: few.pz")
+ */
+public class ปสดหัสกับWeek6เดะมาทำ {
+    
+}
