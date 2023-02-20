221aba72bb0a3be0b18ce2768f17af739ec84a2c
FewPz <33858277+FewPz@users.noreply.github.com>
2023-02-20 15:20:12 +0700

    Update SeniorProgrammer.java

--- src/week5/SeniorProgrammer.java
+++ src/week5/SeniorProgrammer.java
@@ -9,37 +9,38 @@
  */
 public class SeniorProgrammer extends Programmer {
 
-    @Override
+    @Override //Override replace method
     public void coding(String str) {
         if (getEnergy() >= 10) {
             System.out.println("I'm coding about " + str);
-            setEnergy(getEnergy() - 5);
-            setHappiness(getHappiness() - 5);
         } else {
             System.out.println("ZzZzZz");
-            setEnergy(getEnergy() - 5);
-            setHappiness(getHappiness() - 5);
         }
+        //Decrement of energy and happiness with 5 point
+        setEnergy(getEnergy() - 5);
+        setHappiness(getHappiness() - 5);
     }
     
     public void coding(String str, int num) {
         for (int i = 0; i < num; i++) {
+            //Call at method coding(String str) in SeniorProgrammer.
             this.coding(str);
         }
     }
     
     public void coding(char str)  {
-        System.out.println("I'm coding about " + str);
-        setEnergy(getEnergy() - 5);
-        setHappiness(getHappiness() - 5);
+        //Call at method coding(String str) in SeniorProgrammer but using String Object.
+        this.coding(String.valueOf(str));
     }
     
     public void compliment(Programmer p) {
+        //Increment a happiness 5 point of attribute Programmer class. 
         System.out.println(p.getName() + " in a good mood");
         p.setHappiness(p.getHappiness() + 20);
     }
     
     public void blame(Programmer p) {
+        //Decrement a happiness 5 point of attribute Programmer class. 
         System.out.println(p.getName() + " in a bad mood");
         p.setHappiness(p.getHappiness() - 20);
     }
