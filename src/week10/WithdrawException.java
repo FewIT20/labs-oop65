12197eea9b122beffb714ace056e49339e0270a5
EntityKunG <sharpkung4826@gmail.com>
2023-04-10 10:53:45 +0700

    Initial: done week 10 (Expcetion)

---
+++ src/week10/WithdrawException.java
@@ -0,0 +1,17 @@
+/**
+ * You can follow me on instagram!
+ * https://www.instagram.com/few.pz/
+ */
+package week10;
+
+/**
+ *
+ * @author "FewPz (IG: few.pz")
+ */
+public class WithdrawException extends Exception {
+
+    public WithdrawException(String s) {
+        super(s);
+    }
+    
+}
